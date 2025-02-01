97de7a1c8d8dfbd969ab626988c41daedf0aa409
halmi-is <m0000590@naver.com>
2025-02-01 22:37:58 +0900

    1
    1

--- 2차/front/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'vuex'
-
-const user = JSON.parse(localStorage.getItem("user"));
-export default createStore({
-  state: {
-    loggedIn : user!=null? true : false 
-  },
-  
-})
